a783446f9044198e5ec9ba299f042a0dbb8c5b53
Slim Amamou <slim.amamou@gmail.com>
2014-05-18 23:17:14 +0100

    Gets emails from file /etc/rach.mails

--- index.php
+++ index.php
@@ -2,16 +2,20 @@
 <?php
 	session_start();
 	$ini['mail'] = "slim";
-	$ini['url'] = "http://localhost/slim/rach/?otp=";
-	$mails = array('slim');
+	$ini['url'] = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']."?otp=";
+	$ini['maillist'] = "/etc/rach.mails";
+
+	$mails = fopen($ini['maillist'], 'r');
 	if ($_GET['otp'] && $_SESSION['otp'] == $_GET['otp']) {
 		$_SESSION['verified'] = TRUE;
 		unset($_SESSION['otp']);
 	}
 	if ($_SESSION['verified']) {
 		if ($_POST['message']) {
-			foreach ($mails as $m) {
-				mail($m, $_POST['subject'], $_POST['message'], "From: ". $_POST['from']);
+			while ($m = fgets($mails)) {
+				$from = $_POST['from'];
+				$replyto = $_SESSION['from'];
+				mail($m, $_POST['subject'], $_POST['message'], "From: $from\r\nReply-to: $replyto");
 			}
 			unset($_SESSION['verified']);
 		} else {
@@ -29,7 +33,7 @@ Subject <input type='text' name='subject' />
 		if ($_POST['from']) {
 			$_SESSION['from'] = $_POST['from'];
 			$_SESSION['otp']= bin2hex(openssl_random_pseudo_bytes(24));
-			mail($_POST['from'], "Rach - Vérification de votre Email", "Accedez à ce lien pour vérifier votre emai : ".$ini['url'].$_SESSION['otp'], "From: ". $ini['mail']);
+			mail($_POST['from'], "Rach - Vérification de votre Email", "Accedez à ce lien pour vérifier votre email : ".$ini['url'].$_SESSION['otp'], "From: ". $ini['mail']);
 		} else {
 			print "Email <input type='text' name='from' /><button type='submit'>Verifier</button>";
 		}
